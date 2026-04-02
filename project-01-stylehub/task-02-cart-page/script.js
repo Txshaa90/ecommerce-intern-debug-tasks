13895fe8f4a27957f31933f1a59c032898d8fde2
Grenna-patch <patchrebadulla@gmail.com>
2026-04-02 12:26:10 +0800

    Fix: count updater

--- project-01-stylehub/task-02-cart-page/script.js
+++ project-01-stylehub/task-02-cart-page/script.js
@@ -10,7 +10,7 @@ const increaseButton = document.getElementById("increase");
 
 function updateCart() {
   const subtotal = price * quantity;
-  const total = subtotal - shipping;
+  const total = subtotal + shipping;
 
   quantityLabel.textContent = quantity;
   subtotalLabel.textContent = `$${subtotal.toFixed(2)}`;
@@ -18,12 +18,12 @@ function updateCart() {
 }
 
 decreaseButton.addEventListener("click", function () {
-  quantity = quantity - 1;
+ if (quantity > 1) quantity -= 1;
   updateCart();
 });
 
 increaseButton.addEventListener("click", function () {
-  quantity = quantity + 2;
+  quantity += 1;
   updateCart();
 });
 
